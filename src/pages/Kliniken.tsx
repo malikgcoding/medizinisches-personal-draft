d67139a783b28f7e0d26913b13ed41d5ef29ad33
malikgcoding <malikgdesigns@pm.me>
2026-01-24 18:11:15 +0100

    O

--- src/pages/Kliniken.tsx
+++ src/pages/Kliniken.tsx
@@ -13,7 +13,7 @@ const Kliniken = () => {
         <Hero />
 
         {/* Content Sections */}
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 pb-16 space-y-16">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 pt-16 pb-16 space-y-16">
           
           {/* Ausgangslage */}
           <section className="space-y-6">
