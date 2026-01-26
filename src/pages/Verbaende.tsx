742f5048876c4d30daab6d88f53f53072162e17a
malikgcoding <malikgdesigns@pm.me>
2026-01-26 19:37:33 +0100

    Final changes

--- src/pages/Verbaende.tsx
+++ src/pages/Verbaende.tsx
@@ -48,7 +48,7 @@ const Verbaende = () => {
             </div>
                     
           {/* Scroll Indicator */}
-          <div className="absolute bottom-4 left-1/2 -translate-x-1/2 md:hidden">
+          <div className="absolute bottom-2 left-1/2 -translate-x-1/2 md:hidden">
             <svg className="w-6 h-6 text-primary/60" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
             </svg>
